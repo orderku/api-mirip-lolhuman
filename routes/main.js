8bc6af6ec648a5a6d1cf4f4d63d67b564eff170e
orderku <63989360+orderku@users.noreply.github.com>
2022-01-13 20:06:46 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -65,10 +65,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : 'Multi Prefix',
-            namabot: 'ALPHA-BOT',
-            namaowner: 'zeeoneofc',
-            instagram: 'zeeoneofc',
-            github : 'https:\/\/github.com\/zeeoneofc',
+            namabot: 'ARI-BOT',
+            namaowner: 'ARIBOT',
+            instagram: 'ARIBOT',
+            github : 'https:\/\/github.com\/ARIBOT',
         }
     }
     res.json(config)
